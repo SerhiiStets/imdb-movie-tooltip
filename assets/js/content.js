199a906009a25b6e28b551165a3362c00e9f90f3
Serhii Stets <stets.serhii@gmail.com>
2017-07-21 21:08:57 +0300

    Popup settings window
    * add popup window with the tooltip settings
    
    2 modes:
    
    Turn off/on plugin - disable / enable chrome extension
    
    Large tooltip mode - change size of the tooltip

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,68 +1,81 @@
 $().ready(function() {
-
-  add_divs();
-  var timeoutId;
-  $("a").mouseenter(function() {
-    hide();
-    if ($(this).attr('href').indexOf("/title/") !== -1) {
-      if (!timeoutId) {
-        var hov = $(':hover').last();
-        timeoutId = window.setTimeout(function() {
-          timeoutId = null;
-          if (!$(':hover').last().is(hov)) {
-            return;
+  'use strict';
+  getPluginValue(function(value) {
+    if (value.plugin == true) {
+      addDivs();
+      var timeoutId;
+      $("a").mouseenter(function() {
+        hide();
+        if ($(this).attr('href').indexOf("/title/") !== -1) {
+          if (!timeoutId) {
+            var hov = $(':hover').last();
+            timeoutId = window.setTimeout(function() {
+              timeoutId = null;
+              if (!$(':hover').last().is(hov)) {
+                return;
+              }
+              var movie_name = hov.text();
+              var url = (hov.attr('href').indexOf("imdb") !== -1) ? hov.attr('href') : ("http://www.imdb.com" + hov.attr('href'));
+              getData(url, movie_name);
+            }, 700);
           }
-          var movie_name = hov.text();
-          var url = (hov.attr('href').indexOf("imdb") !== -1) ? hov.attr('href') : ("http://www.imdb.com" + hov.attr('href'));
-
-          $.get(url, function(data) {
-            var imdb_score = $('[itemprop=ratingValue]', $(data)).text();
-            var meta_score = $('.metacriticScore', $(data)).text();
-            var year = $('#titleYear', $(data)).text();
-            var image_url = "";
-            var director_name = $('[itemprop=director]', $(data)).text();
-            var actors_names = [];
-
-            $("[itemprop=actors] > [itemprop=url]", $(data)).each(function() {
-              actors_names.push($(this).text());
-            });
-
-            $('.poster img', $(data)).each(function() {
-              image_url = $(this).attr('src');
-            });
-
-            if (imdb_score == "") {
-              imdb_score = "??";
-            }
-
-            if (meta_score == "") {
-              meta_score = "??";
-            }
-            hide();
-            show_movie(movie_name, imdb_score, meta_score, year, image_url, director_name, actors_names);
-          });
-        }, 700);
-      }
+        }
+      }).mouseout(function() {
+        hide();
+      });
     }
-  }).mouseout(function() {
-    hide();
   });
-
 });
 
-function show_movie(name, imdb, meta, year, poster, director, actors) {
-  var color = metascore_color(meta);
+function getPluginValue(callback) {
+  chrome.storage.sync.get("plugin", callback);
+}
 
-  $("#tooltip").css('display', 'block');
+function getTooltipValue(callback) {
+  chrome.storage.sync.get("large_tooltip", callback);
+}
 
-  add_divs();
+function getData(link, name) {
+  $.get(link, function(data) {
+    var imdb_score = $('[itemprop=ratingValue]', $(data)).text();
+    var meta_score = $('.metacriticScore', $(data)).text();
+    var year = $('#titleYear', $(data)).text();
+    var image_url = "";
+    var director_name = $('[itemprop=director]', $(data)).text();
+    var actors_names = [];
+
+    $("[itemprop=actors] > [itemprop=url]", $(data)).each(function() {
+      actors_names.push($(this).text());
+    });
+
+    $('.poster img', $(data)).each(function() {
+      image_url = $(this).attr('src');
+    });
+
+    if (imdb_score == "") {
+      imdb_score = "??";
+    }
+
+    if (meta_score == "") {
+      meta_score = "??";
+    }
+    hide();
+    showMovie(name, imdb_score, meta_score, year, image_url, director_name, actors_names);
+  });
+}
+
+function showMovie(name, imdb, meta, year, poster, director, actors) {
+  var color = metascoreColor(meta);
+
+
+  addDivs();
 
   $('#tooltip').append($('<img/>', {
     id: 'poster',
     src: poster
   }));
 
-  $("#movie_name").append('<p>' + check_movie_name(name) + year + '</p>');
+  $("#movie_name").append('<p>' + checkMovieName(name) + year + '</p>');
 
   $('#imdb_score').append(imdb);
 
@@ -73,11 +86,100 @@ function show_movie(name, imdb, meta, year, poster, director, actors) {
 
   $('#director').append(director);
 
-  $('#actors').append(actors_list(actors));
+  $('#actors').append(actorsList(actors));
 
+  getTooltipValue(function(value) {
+    if (value.large_tooltip == true) {
+      showLargeTooltip();
+    } else {
+      showSmallTooltip();
+    }
+  });
 }
 
-function add_divs() {
+function showLargeTooltip() {
+  var change_val = 300 / 70;
+  var height = 400 / change_val;
+
+  // tooltip
+  $("#tooltip").css('width', 400);
+  $("#tooltip").css('height', height);
+  $("#tooltip").css("font-size", 11 + change_val);
+
+  // poster
+  $("#poster").css('height', height);
+
+  // movie name
+  $("#movie_name").css("left", 80);
+  $("#movie_name").css("bottom", 75);
+  $("#movie_name").css("width", 208 * change_val);
+  $('#movie_name > p:eq(0)').css('font-size', 12 + change_val);
+
+  // director
+  $("#director").css("bottom", 65);
+  $("#director").css("left", 82);
+
+  // actors
+  $("#actors").css("left", 80);
+
+  // line
+  $("#line-separator").css("left", 73);
+  $("#line-separator").css("width", 337);
+  $("#line-separator").css("bottom", 53);
+
+  // imdb score
+  $("#imdb_score").css("left", 380);
+  $("#imdb_score").css("bottom", 90);
+
+  // meta score
+  $("#meta_score").css("left", 382);
+  $("#meta_score").css("bottom", 70);
+
+
+  $("#tooltip").css('display', 'block');
+}
+
+
+function showSmallTooltip() {
+  // tooltip
+  $("#tooltip").css('width', 300);
+  $("#tooltip").css('height', 70);
+  $("#tooltip").css("font-size", 11);
+
+  // poster
+  $("#poster").css('height', 70);
+
+  // movie name
+  $("#movie_name").css("left", 66);
+  $("#movie_name").css("bottom", 63);
+  $("#movie_name").css("width", 208);
+  $('#movie_name > p:eq(0)').css('font-size', 12);
+
+  // director
+  $("#director").css("bottom", 56);
+  $("#director").css("left", 68);
+
+  // actors
+  $("#actors").css("left", 67);
+
+  // line
+  $("#line-separator").css("left", 57);
+  $("#line-separator").css("width", 253);
+  $("#line-separator").css("bottom", 48);
+
+  // imdb score
+  $("#imdb_score").css("left", 288);
+  $("#imdb_score").css("bottom", 73);
+
+  // meta score
+  $("#meta_score").css("left", 290);
+  $("#meta_score").css("bottom", 57);
+
+
+  $("#tooltip").css('display', 'block');
+}
+
+function addDivs() {
   $('body').append($('<div/>', {
     id: 'tooltip'
   }));
@@ -108,7 +210,7 @@ function add_divs() {
   }));
 }
 
-function actors_list(names) {
+function actorsList(names) {
   var list = "";
   names.forEach(function(name, i, array) {
     if (i !== array.length - 1) {
@@ -124,14 +226,12 @@ function actors_list(names) {
     list += "...";
   }
   return list;
-
 }
 
-function check_movie_name(movie) {
+function checkMovieName(movie) {
   if (movie.indexOf("(20") !== -1 || movie.indexOf("(19") !== -1) {
     movie = movie.substring(0, movie.length - 7);
   }
-
   if (movie.length > 33) {
     var value = movie.length - 30;
     movie = movie.substring(0, movie.length - value);
@@ -140,7 +240,7 @@ function check_movie_name(movie) {
   return movie;
 }
 
-function metascore_color(score) {
+function metascoreColor(score) {
   if (score == "??") {
     return "grey";
   } else if (score < 40) {
@@ -158,5 +258,4 @@ function hide() {
   });
   $("#tooltip").empty();
   $("#tooltip").css('display', 'none');
-
 }
